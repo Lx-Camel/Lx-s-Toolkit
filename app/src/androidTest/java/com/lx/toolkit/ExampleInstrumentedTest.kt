e712a3faa129df880e3c672fece17f7a3690763d
liuxin <liuxin@snailgame.net>
2023-04-03 14:41:27 +0800

    [变更内容]：修改代码结构，解决maven编译问题 [作者]：Liux [ID]：无 [影响范围]：无 [限制事项]：无

--- app/src/androidTest/java/com/lx/toolkit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/lx/toolkit/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.lx.tools
+package com.lx.toolkit
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.lx.lx_s_toolkit", appContext.packageName)
+        assertEquals("com.lx.toolkit", appContext.packageName)
     }
 }
